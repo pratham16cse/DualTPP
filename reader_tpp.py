cd9f71e74d9d81d7cc3a5c272e392b00a9e3bdf6
PRATHAMESH SUBODH DESHPANDE <pratham@cse.iitb.ac.in>
2018-12-10 17:07:39 +0530

    Sampling and Saving data to text file.

--- reader_tpp.py
+++ reader_tpp.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 from collections import OrderedDict
 
 import reader
@@ -26,10 +27,35 @@ def createPerDayChunks(latlngList, data):
     return new_data
 
 
+def sample(data):
+    new_data = list()
+    for loc in data:
+        samples = list()
+        for i in range(len(loc)):
+            durn, ts = loc[i]
+            samples_ = np.arange(ts, ts+durn, step=300).tolist()
+            samples += samples_
+        new_data.append(samples)
+        #print(len(loc), loc, samples)
+
+    return new_data
+
+
+def writeToFile(filePath, data):
+    cityName = filePath.split('/')[-1].split('-')[0]
+    with open('../datasets/traffic_tpp/'+cityName+'-congestions.txt', 'w') as f:
+        for loc in data:
+            for ts in loc[:-1]:
+                f.write(str(ts) + ' ')
+            f.write(str(loc[-1]) + '\n')
+
+
 def main():
     filePath = sys.argv[1]
     latlngList, data = reader.read(filePath)
     data = createPerDayChunks(latlngList, data)
+    data = sample(data)
+    writeToFile(filePath, data)
 
 if __name__ == '__main__':
     main()
