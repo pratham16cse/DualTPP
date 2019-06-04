1efe2d98d226f70e0dfa57cb5f988daed14b2808
PRATHAMESH SUBODH DESHPANDE <pratham@cse.iitb.ac.in>
2019-06-04 23:09:26 +0530

    Experiments with Normalization

--- pp_seq2seq/scripy.sh
+++ pp_seq2seq/scripy.sh
@@ -69,6 +69,7 @@ python3.6 \
 	$dataset_path"/dev.time" \
 	$dataset_path"/test.event" \
 	$dataset_path"/test.time" \
+	--normalization "min-max" \
 	--save $save_dir \
 	--cpu-only \
 	--epochs 50 \
