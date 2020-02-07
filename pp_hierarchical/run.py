1cbc0169f9b2509485171e4c21a474bdb9d0a6fa
Kamlesh Marathe <kamleshmarathemits@gmail.com>
2020-02-08 00:40:34 +0530

    Folder structure changed in run file.

--- pp_hierarchical/run.py
+++ pp_hierarchical/run.py
@@ -3,6 +3,7 @@ import argparse
 import run_rmtpp
 import run_hierarchical
 
+import os, sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dataset_name', type=str, help='dataset_name')
@@ -12,12 +13,13 @@ parser.add_argument('alg_path', type=str, help='Path to the code')
 
 parser.add_argument('--output_dir', type=str,
                     help='Path to store all raw outputs, checkpoints, \
-                          summaries, and plots')
+                          summaries, and plots', default='Outputs')
 parser.add_argument('--seed', type=int,
-                    help='Seed for parameter initialization')
-parser.add_argument('--epochs', type=int, default=100,
+                    help='Seed for parameter initialization',
+                    default=42)
+parser.add_argument('--epochs', type=int, default=50,
                     help='number of training epochs')
-parser.add_argument('--patience', type=int, default=0,
+parser.add_argument('--patience', type=int, default=10,
                     help='Number of epochs to wait for \
                           before beginning cross-validation')
 parser.add_argument('--bptt', type=int, default=20,
@@ -40,16 +42,23 @@ parser.add_argument('--learning_rate', type=float, default=1e-2,
 
 parser.add_argument('--generate_plots', action='store_true',
                     help='Generate dev and test plots, both per epochs \
-                          and after training')
+                          and after training', default=True)
 
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-v', '--verbose', action='store_true')
 group.add_argument('-q', '--quite', action='store_true')
 
-
 args = parser.parse_args()
 print(args)
 
+os.makedirs(args.output_dir, exist_ok=True)
+cntr = len(next(os.walk(args.output_dir))[1])
+output_dir = os.path.join(args.output_dir+'/'+str(cntr))
+os.makedirs(output_dir, exist_ok=True)
+args.output_dir = output_dir
+
+with open(args.output_dir+'/params.txt', 'w') as f:
+    f.write(str(args))
 
 if args.alg_name in ['rmtpp']:
     run_rmtpp.run(args)
