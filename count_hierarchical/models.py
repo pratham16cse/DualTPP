time-feature normalization moved to models.py models: 	Added cnt_net_type param to count model. Can take values 'ff' or 'rnn' run: 	minor fix in num_peaks_in_bin: clipped to [1, inf)

count_hierarchical/models.py
@@ -119,6 +119,7 @@ class RMTPP(tf.keras.Model):
         
         # Gather input for the rnn
         if self.use_time_feats:
+            feats = feats/24.
             rnn_inputs = tf.concat([self.gaps, feats], axis=-1)
         else:
             rnn_inputs = self.gaps
@@ -241,13 +242,21 @@ class COUNT_MODEL(tf.keras.Model):
                 out_bin_sz,
                 distribution_name,
                 use_time_feats=True,
+                network_type='ff', # ff or rnn
                 name='count_model',
                 **kwargs):
         super(COUNT_MODEL, self).__init__(name=name, **kwargs)
         self.distribution_name = distribution_name
         self.use_time_feats = use_time_feats
+        self.network_type = network_type
 
-        self.dense1 = tf.keras.layers.Dense(hidden_layer_size, activation=tf.nn.relu, name="count_dense1")
+        if self.network_type == 'ff':
+            self.dense1 = tf.keras.layers.Dense(hidden_layer_size, activation=tf.nn.relu, name="count_dense1")
+        elif self.network_type == 'rnn':
+            self.rnn_layer = layers.GRU(hidden_layer_size, return_sequences=True,
+                                        return_state=True, stateful=False,
+                                        name='GRU_Layer')
+
         self.dense2 = tf.keras.layers.Dense(hidden_layer_size, activation=tf.nn.relu, name="count_dense2")
         self.out_layer = tf.keras.layers.Dense(hidden_layer_size, activation=tf.nn.relu, name="out_layer")
 
@@ -263,10 +272,16 @@ class COUNT_MODEL(tf.keras.Model):
     def call(self, inputs, feats, debug=False):
 
         if self.use_time_feats:
+            feats = feats/24.
             inputs = tf.concat([inputs, feats], axis=-1)
 
-        inputs = tf.reshape(inputs, [inputs.shape[0], -1])
-        hidden_state_1 = self.dense1(inputs)
+        if self.network_type == 'ff':
+            inputs = tf.reshape(inputs, [inputs.shape[0], -1])
+            hidden_state_1 = self.dense1(inputs)
+        elif self.network_type == 'rnn':
+            hidden_states, final_state \
+                = self.rnn_layer(inputs)
+            hidden_state_1 = final_state
         hidden_state_2 = self.dense2(hidden_state_1)
         output_state = self.out_layer(hidden_state_2)
 
@@ -334,6 +349,7 @@ def build_count_model(args, distribution_name):
         out_bin_sz, 
         distribution_name,
         use_time_feats=(not args.no_count_model_feats),
+        network_type=args.cnt_net_type,
     )
     #model.build(input_shape=(batch_size, in_bin_sz))
     optimizer = keras.optimizers.Adam(learning_rate)

count_hierarchical/run.py
@@ -2595,13 +2595,13 @@ def run_rmtpp_with_optimization_fixed_cnt_solver(args, query_models, data, test_
 				be = test_end_hr_bins[batch_idx, dec_idx]
 				bs_cnt = bisect_right(batch_bin_cnrr_cnt_opt_times_pred_uc, bs)
 				be_cnt = bisect_right(batch_bin_cnrr_cnt_opt_times_pred_uc, be)
-				num_peaks_in_bin = be_cnt - bs_cnt
+				num_peaks_in_bin = np.maximum(be_cnt - bs_cnt, 1)
 				if num_peaks_in_bin <= event_count_preds_cnt[batch_idx, dec_idx]:
-					min_cnt = num_peaks_in_bin
+					min_cnt = int(num_peaks_in_bin)
 					max_cnt = int(event_count_preds_cnt[batch_idx, dec_idx] + 1)
 				else:
 					min_cnt = int(event_count_preds_cnt[batch_idx, dec_idx])
-					max_cnt = num_peaks_in_bin + 1
+					max_cnt = int(num_peaks_in_bin + 1)
 
 				if args.use_ratio_constraints:
 					gaps_uc = batch_bin_curr_cnt_opt_gaps_pred_uc